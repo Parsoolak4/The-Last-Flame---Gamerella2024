5b1affafc63ded9abe88c6aa4749ac1c293adc56
mehrdad <muadibbm@yahoo.com>
2024-11-24 14:11:02 -0500

    Player movement is now smooth

--- Assets/Scripts/Data/Unit.cs
+++ Assets/Scripts/Data/Unit.cs
@@ -1,17 +1,24 @@
-using System;
 using UnityEngine;
 
 public class Unit : MonoBehaviour{
 
     [SerializeField] UnitTypes.Movement movements;
+
     public Vector2Int Index { get; set; }
- 
     public UnitTypes.Movement Movements => movements;
+
+    private SpriteRenderer spriteRenderer;
+
+    private void Awake() {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
 
     public void Initialize(UnitTypes.Movement movementData)
     {
         movements = movementData;
     }
     
+    public void SetSortingOrder(int sortingOrder) {
+        spriteRenderer.sortingOrder = sortingOrder;
+    }
 }
-
